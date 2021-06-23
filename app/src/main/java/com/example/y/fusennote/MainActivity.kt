ef4fb82d4d9450de724447869807cb12ec8c958f
Yu357 <forestgreen14689@gmail.com>
2021-06-23 19:13:47 +0900

    ボトムナビゲーションの項目の設定を行った。 第一層の三画面のフラグメントを作成した。

--- app/src/main/java/com/example/y/fusennote/MainActivity.kt
+++ app/src/main/java/com/example/y/fusennote/MainActivity.kt
@@ -15,21 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
     }
 }
