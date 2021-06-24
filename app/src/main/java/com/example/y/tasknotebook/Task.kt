3f1779d2654e122ffa880d9aed02dfc240ed4b34
YuSan357 <forestgreen14689@gmail.com>
2021-06-24 18:35:30 +0900

    テーブル定義編集

--- app/src/main/java/com/example/y/tasknotebook/Task.kt
+++ app/src/main/java/com/example/y/tasknotebook/Task.kt
@@ -7,8 +7,11 @@ open class Task : RealmObject() {
     @PrimaryKey
     var id: Int = 0
     var isAchieved: Boolean = false
-    var achievedYear: Int = 0 //例: 20210615
-    var achievedTime: Int = 0 //例: 091231
+    var achievedYear: Int = 0 //例: 2021, 2022
+    var achievedMonth: Int = 0 //例: 6. 10
+    var achievedDate: Int = 0 //例: 3, 17
+    var achievedHour: Int = 0 //例: 7, 11
+    var achievedMinute: Int = 0 //例: 9, 34
     var isPinned: Boolean = false
     var colorId: Int = 0
     var title: String = ""
