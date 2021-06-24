2edf1620aa32ba12e034cc6467b3c1847ec90563
Yu357 <forestgreen14689@gmail.com>
2021-06-24 09:07:39 +0900

    ファイル名など修正

--- app/src/main/java/com/example/y/tasknotebook/TaskFragment.kt
+++ app/src/main/java/com/example/y/tasknotebook/TaskFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class FusenFragment : Fragment() {
+class TaskFragment : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_fusen, container, false)
+        return inflater.inflate(R.layout.fragment_task, container, false)
     }
 
 
