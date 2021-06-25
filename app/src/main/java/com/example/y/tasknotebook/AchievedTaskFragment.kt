d41949a86d640370378c5b94528b8d69ad246768
YuSan357 <forestgreen14689@gmail.com>
2021-06-25 10:10:34 +0900

    ボトムナビゲーションの項目を変更

--- app/src/main/java/com/example/y/tasknotebook/AchievedTaskFragment.kt
+++ app/src/main/java/com/example/y/tasknotebook/AchievedTaskFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class CalendarFragment : Fragment() {
+class AchievedTaskFragment : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        return inflater.inflate(R.layout.fragment_achieved_task, container, false)
     }
 
 
--- app/src/main/java/com/example/y/tasknotebook/HistoryFragment.kt
+++ app/src/main/java/com/example/y/tasknotebook/HistoryFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class SearchFragment : Fragment() {
+class HistoryFragment : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(R.layout.fragment_history, container, false)
     }
 
 
