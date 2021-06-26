b427641b0b08f5be5d04e9e9147be0b2f4c7087b
Yu357 <forestgreen14689@gmail.com>
2021-06-26 13:31:34 +0900

    OptionalSearchActivityのレイアウトを作成した。

--- app/src/main/java/com/example/y/tasknotebook/OptionalSearchActivity.kt
+++ app/src/main/java/com/example/y/tasknotebook/OptionalSearchActivity.kt
@@ -2,10 +2,21 @@ package com.example.y.tasknotebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_optional_search.*
 
 class OptionalSearchActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_optional_search)
+
+        //backButton03を押すと、finish
+        backButton03.setOnClickListener {
+            finish()
+        }
+
     }
+
+
 }
