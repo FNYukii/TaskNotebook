771011563cfccf8a2626682a9a4c4cc183ab6732
Yu357 <forestgreen14689@gmail.com>
2021-06-27 23:40:51 +0900

    tileが必ず正方形になるようにした。

---
+++ app/src/main/java/com/example/y/tasknotebook/SquareConstraintLayout.kt
@@ -0,0 +1,16 @@
+package com.example.y.tasknotebook
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class SquareConstraintLayout: ConstraintLayout {
+    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        //widthとheightを同じ値にする
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
